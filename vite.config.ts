82d5a371422f5cfa50d52054875809e13a34f3f1
Sai Akhila <saiakhilandeshwari@gmail.com>
2026-07-29 22:49:47 +0530

    Update1 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/saiakhila-rizzgithub.github.io/",
+  base: "/",
 
   server: {
     host: "::",
